91f7313309279010dc3116fcf4db3a322ae5d5b5
Erick <erickavendao014@gmail.com>
2026-06-04 17:09:27 -0500

    Servicio de Doctores

---
+++ doctores-service/src/main/java/com/SmartHealth/doctores_service/model/Doctor.java
@@ -0,0 +1,26 @@
+package com.SmartHealth.doctores_service.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer idDoctor;
+    public String nombres;
+    public String especialidad;
+    public Boolean disponibilidad;
+    public Boolean estado;
+
+
+}
